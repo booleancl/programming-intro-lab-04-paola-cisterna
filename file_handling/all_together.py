5610608807e7f1fa5629e0ca04097c043f5c0a4d
aylinsanmartin <a.sanlazo@gmail.com>
2022-09-21 11:01:47 -0300

    refactirizar

--- file_handling/all_together.py
+++ file_handling/all_together.py
@@ -2,7 +2,18 @@
 print("bienvenido al programa")
 user_input = ""
 
-while user_input != "exit":
+def write_file():
+     file = open("file_handling/demo_two.txt","a")
+     user_content = input("ingresa el contenido\n")
+     file.write(user_content + "\n")
+     file.close()
+
+def read_file():
+     file = open ("file_handling/demo_two.txt", "r" )
+     for line in file:
+          print(line)
+
+def print_menu():
     print("##############")
     print("ingresa una opcion")
     print("1","agregar contenido")
@@ -10,19 +21,19 @@ while user_input != "exit":
     print("exit", "para salir")
     print("################")
 
+
+while user_input != "exit":
+    print_menu()
     user_input = input()
     
     if user_input == "1":
-         file = open("demo_two.txt","a")
-         user_content = input("ingresa el contenido")
-         file_write(user_content + "\n")
-         file.close()
-
+          write_file()
     elif user_input == "2":
-         file = open ("demo_two.txt", "r" )
-         for line in file:
-             print(line)
+         read_file()
+    elif user_input == "exit":
+          print("chau chau")
+          exit()         
     else:
          print("opcion incorrecta")
 
-print("chau chau")
+
