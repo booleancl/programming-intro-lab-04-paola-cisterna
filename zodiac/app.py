605b7fda41c976d40eb9881412d326d9f9863237
aylinsanmartin <a.sanlazo@gmail.com>
2022-09-24 12:01:06 -0300

    cachipun 2.0

---
+++ zodiac/app.py
@@ -0,0 +1,21 @@
+
+print("bienvenido al horoscopo")
+
+def read_file (name):
+    file name = "zodiac/aries.txt"
+    file = open(file_aries,"r")
+    for line in file:
+        print(line)
+
+
+print_menu():
+print("ingresa tu opcion")
+
+  print("1","Aries")
+  print("2","Sagitario")
+  print("3","Capricornio")
+  print("4", "Tauro")
+  print("5", "leo")
+  
+
+read_file(aries)    
